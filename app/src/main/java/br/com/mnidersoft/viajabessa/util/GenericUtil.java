10c07b2a1abafabc4affa2495f435e2993272525
Allan Ariel Leite Menezes Santos <allan.ariel1987@gmail.com>
2015-08-17 13:59:00 -0300

    no message

--- app/src/main/java/br/com/mnidersoft/viajabessa/util/GenericUtil.java
+++ app/src/main/java/br/com/mnidersoft/viajabessa/util/GenericUtil.java
@@ -1,21 +1,14 @@
 package br.com.mnidersoft.viajabessa.util;
 
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
-import android.telephony.TelephonyManager;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 import br.com.mnidersoft.viajabessa.R;
 
-/**
- * Created by institutoeducar on 26/07/15.
- */
 public class GenericUtil {
 
     public static boolean isOnline(Context context) throws Exception {
